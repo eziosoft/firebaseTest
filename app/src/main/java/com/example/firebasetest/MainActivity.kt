aec4d55076c001a7f9e0a17566a8fd100e27541b
Bart <eziosoft@gmail.com>
2021-07-08 12:39:12 +0200

    ..

--- app/src/main/java/com/example/firebasetest/MainActivity.kt
+++ app/src/main/java/com/example/firebasetest/MainActivity.kt
@@ -4,46 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.firebase.ui.auth.util.ExtraConstants
 import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-//    private val signInLauncher = registerForActivityResult(
-//        FirebaseAuthUIActivityResultContract()
-//    ) { res ->
-//        this.onSignInResult(res)
-//    }
+    private lateinit var auth: FirebaseAuth
 
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-    }
-
-    fun createSignInIntent() {
-
-
-        // Choose authentication providers
-        val providers = arrayListOf(
-//            AuthUI.IdpConfig.EmailBuilder().build(),
-//            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build()
-//            AuthUI.IdpConfig.FacebookBuilder().build(),
-//            AuthUI.IdpConfig.TwitterBuilder().build()
-        )
-
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        launch(signInIntent)
-
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
     }
 
 
@@ -53,8 +35,68 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button2)
 
-        button.setOnClickListener(){
+        button.setOnClickListener() {
             createSignInIntent()
+        }
+
+        findViewById<Button>(R.id.buttonLogout).setOnClickListener {
+            signOut()
+        }
+
+        findViewById<Button>(R.id.buttonDelete).setOnClickListener {
+            delete()
+        }
+
+        auth = Firebase.auth
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Toast.makeText(this, "Not Logged in", Toast.LENGTH_SHORT).show()
+
+        } else {
+            Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show()
+            updateScreen(currentUser)
+        }
+    }
+
+    fun createSignInIntent() {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+//            AuthUI.IdpConfig.FacebookBuilder().build(),
+//            AuthUI.IdpConfig.TwitterBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.fui_ic_github_white_24dp) // Set logo drawable
+            .setTheme(R.style.Base_ThemeOverlay_AppCompat_Light) // Set theme
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+
+    private fun updateScreen(user: FirebaseUser?) {
+        user?.let {
+            var s = ""
+            s += "name:" + it.displayName + "\n"
+            s += "email:" + it.email + "\n"
+            s += "email verified:" + it.isEmailVerified.toString() + "\n"
+            s += "phone:" + it.phoneNumber + "\n"
+            s += "photoURL:" + it.photoUrl.toString() + "\n"
+            s += "providerID:" + it.providerId + "\n"
+            s += "UID:" + it.uid + "\n"
+            findViewById<TextView>(R.id.textView).text = s
+
+            Glide.with(this).load(it.photoUrl).into(findViewById(R.id.imageView))
         }
     }
 
@@ -64,12 +106,9 @@ class MainActivity : AppCompatActivity() {
         if (result.resultCode == RESULT_OK) {
             // Successfully signed in
             val user = FirebaseAuth.getInstance().currentUser
-            // ...
+            updateScreen(user)
         } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
+            Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -78,7 +117,7 @@ class MainActivity : AppCompatActivity() {
         AuthUI.getInstance()
             .signOut(this)
             .addOnCompleteListener {
-                // ...
+                Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show()
             }
         // [END auth_fui_signout]
     }
@@ -94,20 +133,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun themeAndLogo() {
-        val providers = emptyList<AuthUI.IdpConfig>()
-
-        // [START auth_fui_theme_logo]
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .setLogo(R.drawable.fui_ic_github_white_24dp) // Set logo drawable
-            .setTheme(R.style.Base_ThemeOverlay_AppCompat_Light) // Set theme
-            .build()
-        signInLauncher.launch(signInIntent)
-        // [END auth_fui_theme_logo]
-    }
-
     private fun privacyAndTerms() {
         val providers = emptyList<AuthUI.IdpConfig>()
         // [START auth_fui_pp_tos]
@@ -116,7 +141,8 @@ class MainActivity : AppCompatActivity() {
             .setAvailableProviders(providers)
             .setTosAndPrivacyPolicyUrls(
                 "https://example.com/terms.html",
-                "https://example.com/privacy.html")
+                "https://example.com/privacy.html"
+            )
             .build()
         signInLauncher.launch(signInIntent)
         // [END auth_fui_pp_tos]
@@ -128,7 +154,8 @@ class MainActivity : AppCompatActivity() {
             .setAndroidPackageName( /* yourPackageName= */
                 "...",  /* installIfNotAvailable= */
                 true,  /* minimumVersion= */
-                null)
+                null
+            )
             .setHandleCodeInApp(true) // This must be set to true
             .setUrl("https://google.com") // This URL needs to be whitelisted
             .build()
